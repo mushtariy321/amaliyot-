097b435f7eb725148e3af1f97f8c976b13b94387
mushtariy321 <mushtariyrixsiboyev5a@gmail.com>
2024-05-31 10:27:32 +0300

    done

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// let input = document.querySelector('input')
+// let btn = document.querySelector('button')
+// let img = document.querySelector('img')
+
+// btn.addEventListener('click', () => {
+//     img.setAttribute('src', input.value)
+// })
+let fon = prompt('Orqa fon tanlang')
+let tex = prompt('Text rangini tanlang')
+
+let Body = document.querySelector('body')
+let text = document.querySelector('.p')
+
+Body.style.backgroundColor = first
+text.style.color = second
